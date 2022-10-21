4e1ea168773cde9101fcccfa9e8ccb308e15eae2
ranjith.poojari <ranjith.poojary@niveussolutions.com>
2022-10-21 19:13:56 +0530

    Demo Project...

---
+++ demo_project/src/test/java/com/niveus/demo_project/DemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niveus.demo_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
